477f9ad94f1260bc87d25fff12154612765fb3a8
pierangeloc <pierangeloc@gmail.com>
2016-03-21 23:03:19 +0100

    Initial commit

---
+++ src/main/java/com/pierangeloc/Person.java
@@ -0,0 +1,39 @@
+package com.pierangeloc;
+
+public class Person {
+
+    private String name;
+    private String surname;
+    private String discipline;
+
+    public Person(String name, String surname, String discipline) {
+        this.name = name;
+        this.surname = surname;
+        this.discipline = discipline;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDiscipline() {
+        return discipline;
+    }
+
+    public void setDiscipline(String discipline) {
+        this.discipline = discipline;
+    }
+
+}
---
+++ src/main/scala/com/pierangeloc/Books.scala
@@ -0,0 +1,31 @@
+package com.pierangeloc
+
+import scala.io.Source
+import scala.util.matching.Regex
+
+case class Book(id: Int, title: String, authors: List[String], topic: String)
+
+object BooksRepository {
+  private val ratingsPath = "/home/pierangeloc/Documents/projects/scala/presentations/scala-gentle-intro/src/main/resources/programming-books-ratings.txt"
+  private val booksPath = "/home/pierangeloc/Documents/projects/scala/presentations/scala-gentle-intro/src/main/resources/programming-books.txt"
+
+  val BookRow: Regex = "([^\\|].*)\\|([^\\|]*)\\|([^\\|]*)".r
+  val RatingRow = "([^\\|].*)\\|([^\\|]*)".r
+
+  def load(path: String = booksPath): List[Book] =
+    Source.fromFile(path)
+      .getLines().map{
+    case BookRow(title, authors, topic) => (title, authors.split(",").toList, topic)
+  }.toList.zipWithIndex.map{
+    case ((title, author, topic), index) => Book(index + 1, title, author, topic)
+  }
+
+  lazy val books: Map[Int, Book] = load().map(book => (book.id, book)).toMap
+
+  lazy val ratings: Map[Int, Int] =
+    Source.fromFile(ratingsPath)
+        .getLines().map {
+      case RatingRow(id, rating) => (id.toInt, rating.toInt)
+    }.toMap
+
+}
